0015e488abbcfd3ba7111086041a223900bbf715
giovannipastorini <giovannipastorini9@gmail.com>
2026-05-09 12:55:19 +0200

    Add blogPosts data for blog post functionality

---
+++ src/data/app_data.js
@@ -0,0 +1,28 @@
+const blogPosts = [
+  {
+    author: "Mario Rossi",
+    title: "Introduzione a JavaScript",
+    body: "JavaScript è un linguaggio di programmazione molto usato per il web...",
+    public: true
+  },
+  {
+    author: "Giulia Bianchi",
+    title: "Guida a React",
+    body: "React è una libreria per costruire interfacce utente dinamiche...",
+    public: true
+  },
+  {
+    author: "Luca Verdi",
+    title: "Concetti base di programmazione",
+    body: "In questo articolo vediamo le basi della programmazione...",
+    public: false
+  },
+  {
+    author: "Anna Neri",
+    title: "Come funzionano le API",
+    body: "Le API permettono a due sistemi di comunicare tra loro...",
+    public: true
+  }
+];
+
+export {blogPosts}
